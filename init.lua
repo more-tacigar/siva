2e5fba4bcb7542bba3c15af533e730a193941abe
tacigar <ig.lhound@gmail.com>
2016-09-30 23:02:45 +0900

    [ADD] Add init.lua and api.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+------------------------------------------------------------
+-- Copyright (c) 2016 tacigar. All rights reserved.
+-- https://github.com/tacigar/maidroid
+------------------------------------------------------------
+
+siva = {}
+
+siva.modname = "siva"
+siva.modpath = minetest.get_modpath(siva.modname)
+
+dofile(siva.modpath .. "/api.lua")
